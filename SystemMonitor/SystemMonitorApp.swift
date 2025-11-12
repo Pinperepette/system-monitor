eb71fb8d7b1c5d3e44160d340b702fe52ca0d797
Pinperepette <pinperepette@gmail.com>
2025-11-12 20:43:32 +0100

    Initial Commit

---
+++ SystemMonitor/SystemMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemMonitorApp.swift
+//  SystemMonitor
+//
+//  Created by Amani Andrea on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SystemMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
